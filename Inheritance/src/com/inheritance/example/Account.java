1a2c504c28b744fe041c88a0c142542027f0673a
Hameed <raashid-umar-haris.hameed@capgemini.com>
2023-05-22 11:45:38 +0530

    Java Inheritance Folder Added

---
+++ Inheritance/src/com/inheritance/example/Account.java
@@ -0,0 +1,31 @@
+package com.inheritance.example;
+
+public class Account {
+	long accNum;
+	double balance;
+	Person  accHolder;
+	public long getAccNum() {
+		return accNum;
+	}
+	public void setAccNum(long accNum) {
+		this.accNum = accNum;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public Person getAccHolder() {
+		return accHolder;
+	}
+	public void setAccHolder(Person accHolder) {
+		this.accHolder = accHolder;
+	}
+	public void deposit(double balance)
+	{
+		balance=balance;
+	}
+	
+
+}
---
+++ Inheritance/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rhameed
+ *
+ */
+module Inheritance {
+}
